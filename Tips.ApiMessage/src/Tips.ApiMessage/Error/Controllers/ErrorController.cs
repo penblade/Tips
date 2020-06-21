92cc4ddce820399a08b6847ab25fde658021f443
Andrew Hinkle <penbladellc@gmail.com>
2020-06-20 23:59:21 -0400

    Moved the global exception handler to a controller following the Microsoft docs.

---
+++ Tips.ApiMessage/src/Tips.ApiMessage/Error/Controllers/ErrorController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Net;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Tips.ApiMessage.Contracts;
+
+namespace Tips.ApiMessage.Error.Controllers
+{
+    [ApiController]
+    public class ErrorController : Controller
+    {
+        // https://docs.microsoft.com/en-us/aspnet/core/web-api/handle-errors?view=aspnetcore-3.1
+        [Route("/error")]
+        public IActionResult Error()
+        {
+            var context = HttpContext.Features.Get<IExceptionHandlerFeature>();
+            var exception = context.Error;
+
+            // ProblemDetails implements the RF7807 standards.
+            // TraceId is already returned.
+            var problemDetails = new ProblemDetails
+            {
+                Title = "Internal Server Error",
+                Status = (int)HttpStatusCode.InternalServerError,
+                Detail = "Internal Server Error",
+                Instance = $"urn:{CompanyConstants.UrnName}:error:{Guid.NewGuid()}",
+            };
+
+            // TODO: log the exception
+
+            return Problem(problemDetails.Detail, problemDetails.Instance, problemDetails.Status, problemDetails.Title, problemDetails.Type);
+        }
+    }
+}
--- Tips.ApiMessage/src/Tips.ApiMessage/Startup.cs
+++ Tips.ApiMessage/src/Tips.ApiMessage/Startup.cs
@@ -1,14 +1,8 @@
-using System;
-using System.Net;
 using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.EntityFrameworkCore;
-using Tips.ApiMessage.Contracts;
 using Tips.ApiMessage.Handlers;
 using Tips.ApiMessage.TodoItems.Context;
 using Tips.ApiMessage.TodoItems.CreateTodoItems;
@@ -16,7 +10,6 @@ using Tips.ApiMessage.TodoItems.DeleteTodoItems;
 using Tips.ApiMessage.TodoItems.GetTodoItem;
 using Tips.ApiMessage.TodoItems.GetTodoItems;
 using Tips.ApiMessage.TodoItems.UpdateTodoItem;
-using System.Text.Json;
 
 namespace Tips.ApiMessage
 {
@@ -50,30 +43,7 @@ namespace Tips.ApiMessage
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            app.UseExceptionHandler(errorApp =>
-            {
-                // https://www.strathweb.com/2018/07/centralized-exception-handling-and-request-validation-in-asp-net-core/
-                errorApp.Run(async context =>
-                {
-                    var errorFeature = context.Features.Get<IExceptionHandlerFeature>();
-                    var exception = errorFeature.Error;
-
-                    // ProblemDetails implements the RF7807 standards.
-                    var problemDetails = new ProblemDetails
-                    {
-                        Title = "Internal Server Error",
-                        Status = (int)HttpStatusCode.InternalServerError,
-                        Detail = "Internal Server Error",
-                        // Generic organizational URI
-                        Instance = $"urn:{CompanyConstants.UrnName}:error:{Guid.NewGuid()}" // change the guid to the TraceId.
-                    };
-
-                    // TODO: log the exception
-
-                    context.Response.ContentType = "application/problem+json";
-                    await context.Response.WriteAsync(JsonSerializer.Serialize(problemDetails, new JsonSerializerOptions { WriteIndented = true }));
-                });
-            });
+            app.UseExceptionHandler("/error");
 
             app.UseHttpsRedirection();
 
