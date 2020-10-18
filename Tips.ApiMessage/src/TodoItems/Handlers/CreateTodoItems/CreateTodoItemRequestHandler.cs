09a7920039a710e4de95ac5245c2d543ba999fc0
Andrew Hinkle <penbladellc@gmail.com>
2020-10-18 13:49:14 -0400

    Tips.ApiMessage - Log the todoItemEntityResponse just before persisting, so we know exactly what was saved at this point in time.

--- Tips.ApiMessage/src/TodoItems/Handlers/CreateTodoItems/CreateTodoItemRequestHandler.cs
+++ Tips.ApiMessage/src/TodoItems/Handlers/CreateTodoItems/CreateTodoItemRequestHandler.cs
@@ -1,6 +1,8 @@
 ﻿using System.Linq;
+using System.Text.Json;
 using System.Threading;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
 using Tips.Pipeline;
 using Tips.Rules;
 using Tips.TodoItems.Context.Models;
@@ -14,14 +16,17 @@ namespace Tips.TodoItems.Handlers.CreateTodoItems
         private readonly IRulesEngine _todoItemRulesEngine;
         private readonly IRulesFactory<Request<TodoItem>, Response<TodoItemEntity>> _saveRulesFactory;
         private readonly ICreateTodoItemRepository _createTodoItemRepository;
+        private readonly ILogger<CreateTodoItemRequestHandler> _logger;
 
         public CreateTodoItemRequestHandler(IRulesEngine todoItemRulesEngine,
             IRulesFactory<Request<TodoItem>, Response<TodoItemEntity>> saveRulesFactory,
-            ICreateTodoItemRepository createTodoItemRepository)
+            ICreateTodoItemRepository createTodoItemRepository,
+            ILogger<CreateTodoItemRequestHandler> logger)
         {
             _todoItemRulesEngine = todoItemRulesEngine;
             _saveRulesFactory = saveRulesFactory;
             _createTodoItemRepository = createTodoItemRepository;
+            _logger = logger;
         }
 
         public async Task<Response<TodoItem>> HandleAsync(CreateTodoItemRequest request, CancellationToken cancellationToken)
@@ -33,9 +38,12 @@ namespace Tips.TodoItems.Handlers.CreateTodoItems
             if (todoItemEntityResponse.HasErrors()) return new Response<TodoItem>(todoItemEntityResponse.Notifications);
 
             // Command.  Save the data.
+            _logger.LogInformation(CreateLogMessage(JsonSerializer.Serialize(todoItemEntityResponse)));
             await _createTodoItemRepository.SaveAsync(todoItemEntityResponse, cancellationToken);
 
             return new Response<TodoItem>(todoItemEntityResponse.Notifications, TodoItemMapper.MapToTodoItem(todoItemEntityResponse.Item));
         }
+
+        private static string CreateLogMessage(string response) => @$"TraceId: {Tracking.TraceId} | Created: {LogFormatter.FormatForLogging(response)}";
     }
 }
--- Tips.ApiMessage/src/TodoItems/Handlers/UpdateTodoItem/UpdateTodoItemRequestHandler.cs
+++ Tips.ApiMessage/src/TodoItems/Handlers/UpdateTodoItem/UpdateTodoItemRequestHandler.cs
@@ -1,6 +1,8 @@
 ﻿using System.Linq;
+using System.Text.Json;
 using System.Threading;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
 using Tips.Pipeline;
 using Tips.Rules;
 using Tips.TodoItems.Context.Models;
@@ -14,34 +16,40 @@ namespace Tips.TodoItems.Handlers.UpdateTodoItem
         private readonly IRulesFactory<Request<TodoItem>, Response<TodoItemEntity>> _saveRulesFactory;
         private readonly IRulesFactory<UpdateTodoItemRequest, Response<TodoItemEntity>> _updateRulesFactory;
         private readonly IUpdateTodoItemRepository _updateTodoItemRepository;
+        private readonly ILogger<UpdateTodoItemRequestHandler> _logger;
 
         public UpdateTodoItemRequestHandler(IRulesEngine rulesEngine,
             IRulesFactory<Request<TodoItem>, Response<TodoItemEntity>> saveRulesFactory,
             IRulesFactory<UpdateTodoItemRequest, Response<TodoItemEntity>> updateRulesFactory,
-            IUpdateTodoItemRepository updateTodoItemRepository)
+            IUpdateTodoItemRepository updateTodoItemRepository,
+            ILogger<UpdateTodoItemRequestHandler> logger)
         {
             _rulesEngine = rulesEngine;
             _saveRulesFactory = saveRulesFactory;
             _updateRulesFactory = updateRulesFactory;
             _updateTodoItemRepository = updateTodoItemRepository;
+            _logger = logger;
         }
 
         public async Task<Response> HandleAsync(UpdateTodoItemRequest request, CancellationToken cancellationToken)
         {
-            var todoItemResponse = new Response<TodoItemEntity>();
+            var todoItemEntityResponse = new Response<TodoItemEntity>();
 
             // Query. Apply all validation and modification rules.  These rules can only query the database.
-            await _rulesEngine.ProcessAsync(request, todoItemResponse, _updateRulesFactory.Create().ToList());
-            if (todoItemResponse.HasErrors()) return new Response(todoItemResponse.Notifications);
+            await _rulesEngine.ProcessAsync(request, todoItemEntityResponse, _updateRulesFactory.Create().ToList());
+            if (todoItemEntityResponse.HasErrors()) return new Response(todoItemEntityResponse.Notifications);
 
             // Only process the save rules if the update rules passed with no errors.
-            await _rulesEngine.ProcessAsync(request, todoItemResponse, _saveRulesFactory.Create().ToList());
-            if (todoItemResponse.HasErrors()) return new Response(todoItemResponse.Notifications);
+            await _rulesEngine.ProcessAsync(request, todoItemEntityResponse, _saveRulesFactory.Create().ToList());
+            if (todoItemEntityResponse.HasErrors()) return new Response(todoItemEntityResponse.Notifications);
 
             // Command.  Save the data.
-            await _updateTodoItemRepository.SaveAsync(todoItemResponse, cancellationToken);
+            _logger.LogInformation(CreateLogMessage(JsonSerializer.Serialize(todoItemEntityResponse)));
+            await _updateTodoItemRepository.SaveAsync(todoItemEntityResponse, cancellationToken);
 
-            return new Response(todoItemResponse.Notifications);
+            return new Response(todoItemEntityResponse.Notifications);
         }
+
+        private static string CreateLogMessage(string response) => @$"TraceId: {Tracking.TraceId} | Updated: {LogFormatter.FormatForLogging(response)}";
     }
 }
