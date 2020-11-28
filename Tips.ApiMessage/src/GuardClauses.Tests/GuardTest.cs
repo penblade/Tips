9f7c799d0f9a19f50a20460f766253c2842b2b9c
Andrew Hinkle <penbladellc@gmail.com>
2020-11-28 17:01:46 -0500

    Tips.ApiMessage - Added GuardClauses.Tests project

---
+++ Tips.ApiMessage/src/GuardClauses.Tests/GuardTest.cs
@@ -0,0 +1,55 @@
+using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Tips.GuardClauses;
+
+namespace GuardClauses.Tests
+{
+    [TestClass]
+    public class GuardTest
+    {
+        [TestMethod]
+        [DynamicData(nameof(SetupAgainstNullThrowsException), DynamicDataSourceType.Method)]
+        public void AgainstNullThrowsException(object input, string parameterName)
+        {
+            var expectedException = new ArgumentNullException(parameterName);
+            var actualException = Assert.ThrowsException<ArgumentNullException>(() => Guard.AgainstNull(input, parameterName));
+            Assert.AreEqual(expectedException.Message, actualException.Message);
+        }
+
+        private static IEnumerable<object[]> SetupAgainstNullThrowsException()
+        {
+            var anonymous = new {};
+            anonymous = null;
+
+            yield return new object[] { null, "null" };
+            yield return new object[] { anonymous, "anonymousObject" };
+            yield return new object[] { (Tuple<string, int>) null, "tuple" };
+            yield return new object[] { (string) null, "string" };
+            yield return new object[] { (int?) null, "nullableInt" };
+            yield return new object[] { (bool?) null, "nullableBool" };
+            yield return new object[] { (Exception) null, "exception" };
+            yield return new object[] { (List<string>) null, "stringList" };
+        }
+
+        [TestMethod]
+        [DynamicData(nameof(SetupAgainstNullDoesNothingTest), DynamicDataSourceType.Method)]
+        public void AgainstNullDoesNothingTest(object input, string parameterName)
+        {
+            Guard.AgainstNull(input, parameterName);
+        }
+
+        private static IEnumerable<object[]> SetupAgainstNullDoesNothingTest()
+        {
+            var anonymous = new { };
+
+            yield return new object[] { anonymous, "anonymousObject" };
+            yield return new object[] { new Tuple<string, int>("test", 5), "tuple" };
+            yield return new object[] { "null", "string" };
+            yield return new object[] { (int?) 1, "nullableInt" };
+            yield return new object[] { (bool?) false, "nullableBool" };
+            yield return new object[] { new Exception("testException"), "exception" };
+            yield return new object[] { new List<string>(), "stringList" };
+        }
+    }
+}
