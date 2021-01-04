fb005b0e4f1b0c8d630d6ae8d4f9408a740c041a
Andrew Hinkle <penbladellc@gmail.com>
2021-01-03 21:41:57 -0500

    Tips.ApiMessage - Added Pipeline.Tests\Logging\LoggingBehaviorTest.cs.  Added MockLogger Support class.

---
+++ Tips.ApiMessage/src/Pipeline.Tests/Logging/LoggingBehaviorTest.cs
@@ -0,0 +1,56 @@
+using System.Text.Json;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using Support.Tests;
+using Tips.Pipeline;
+using Tips.Pipeline.Logging;
+
+namespace Pipeline.Tests.Logging
+{
+    [TestClass]
+    public class LoggingBehaviorTest
+    {
+        [TestMethod]
+        public async Task HandleAsyncTest()
+        {
+            const string requestString = "Request";
+            const string responseString = "Response";
+
+            var fakeRequest = new FakeRequest { FakeIntProperty = 42, FakeStringProperty = "Test String" };
+            var serializedFakeRequest = JsonSerializer.Serialize(fakeRequest);
+
+            var fakeResponse = new FakeResponse { FakeBoolProperty = true, FakeDecimalProperty = 3.6M };
+            var serializedFakeResponse = JsonSerializer.Serialize(fakeResponse);
+
+            var mockLogger = new Mock<ILogger<LoggingBehavior>>();
+
+            //var loggingBehavior = new LoggingBehavior(fakeLogger);
+            var loggingBehavior = new LoggingBehavior(mockLogger.Object);
+            var mockRequestHandlerDelegate = new Mock<RequestHandlerDelegate<FakeResponse>>();
+            mockRequestHandlerDelegate.Setup(x => x.Invoke()).ReturnsAsync(fakeResponse);
+
+            var actualResponse = await loggingBehavior.HandleAsync(fakeRequest, mockRequestHandlerDelegate.Object);
+
+            MockLogger.VerifyBeginScope(mockLogger, requestString);
+            MockLogger.VerifyBeginScope(mockLogger, responseString);
+            MockLogger.VerifyLog(mockLogger, LogLevel.Information, serializedFakeRequest, requestString);
+            MockLogger.VerifyLog(mockLogger, LogLevel.Information, serializedFakeResponse, responseString);
+
+            Assert.AreSame(fakeResponse, actualResponse);
+        }
+
+        public class FakeRequest
+        {
+            public int FakeIntProperty { get; set; }
+            public string FakeStringProperty { get; set; }
+        }
+
+        public class FakeResponse
+        {
+            public bool FakeBoolProperty { get; set; }
+            public decimal FakeDecimalProperty { get; set; }
+        }
+    }
+}
---
+++ Tips.ApiMessage/src/Support.Tests/MockLogger.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Extensions.Logging;
+using Moq;
+
+namespace Support.Tests
+{
+    public class MockLogger
+    {
+        public static void VerifyBeginScope<TCategory>(Mock<ILogger<TCategory>> mockLogger, string state) =>
+            mockLogger.Verify(logger => logger.BeginScope(It.Is<object>(request => request.ToString() == state)), Times.Once);
+
+        public static void VerifyLog<TCategory>(Mock<ILogger<TCategory>> mockLogger, LogLevel logLevel, string message, string key) =>
+            mockLogger.Verify(logger =>
+                    logger.Log(
+                        logLevel,
+                        It.IsAny<EventId>(),
+                        It.Is<It.IsAnyType>((state, t) => CheckValue(state, message, key)),
+                        It.IsAny<Exception>(),
+                        (Func<It.IsAnyType, Exception, string>)It.IsAny<object>())
+                , Times.Once);
+
+        /// <summary>
+        /// Checks that a given key exists in the given collection, and that the value matches
+        /// *** Updated to get FirstOrDefault. ***
+        /// Credit: https://christianfindlay.com/2020/07/03/ilogger/
+        /// Code: https://github.com/MelbourneDeveloper/Samples/blob/650ba4bc6cba631da651c2c6732bce337e6a7d8e/ILoggerSamples/ILoggerSamples/ILoggerTests.cs#L44
+        /// </summary>
+        private static bool CheckValue(object state, object expectedValue, string key)
+        {
+            var keyValuePairList = (IReadOnlyList<KeyValuePair<string, object>>)state;
+
+            var actualValue = keyValuePairList.FirstOrDefault(kvp =>
+                string.Compare(kvp.Key, key, StringComparison.Ordinal) == 0).Value;
+
+            return expectedValue.Equals(actualValue);
+        }
+    }
+}
