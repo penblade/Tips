767bc89e499d774605850c642f4443994874a37f
Andrew Hinkle <penbladellc@gmail.com>
2021-09-12 20:12:53 -0400

    Tips.ApiMessage - Api.csproj - enforce GenerateDocumentationFile and NoWarn to all configurations, not just Debug.

--- Tips.ApiMessage/src/Swagger/Extensions/SwaggerExtensions.cs
+++ Tips.ApiMessage/src/Swagger/Extensions/SwaggerExtensions.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Reflection;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.OpenApi.Models;
